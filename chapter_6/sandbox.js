bd30facab3032658ff3c0e3bb3811f562297514f
Vincent Mos <vincenzomos@gmail.com>
2019-07-16 11:05:28 +0200

    custom stuff for lesson 47

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,6 +1,9 @@
 // get an element by ID
 const title= document.getElementById('page-title');
+const title2 = document.querySelector('#page-title');
 console.log(title);
+console.log(title2);
+
 
 // get elements by their class name
 const errors = document.getElementsByClassName('error');
@@ -11,3 +14,6 @@ console.log(errors[0]);
 const paras = document.getElementsByTagName('p');
 console.log(paras);
 console.log(paras[1]);
+
+const paras2 = document.querySelectorAll('p');
+console.log(paras2);
