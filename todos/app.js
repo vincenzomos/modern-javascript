6c2ca48328d29cc3ca1ab31f76ac949b2924bb00
Vincent Mos <vincenzomos@gmail.com>
2019-07-18 10:33:33 +0200

    Result of ToDo app by writing with instructor

--- todos/app.js
+++ todos/app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector('.add');
 const list = document.querySelector('.todos');
+const search = document.querySelector('.search input')
 
 const generateTemplate = todo => {
   const html = `
@@ -22,3 +23,32 @@ addForm.addEventListener('submit', e => {
   }
 
 });
+
+//delete todos
+list.addEventListener('click', e => {
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+    }
+});
+
+
+const filterTodos = (term) => {
+    console.log(term);
+    Array.from(list.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.add('filtered'));
+
+    Array.from(list.children)
+    .filter(todo => todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.remove('filtered'));
+}
+
+//filter function
+search.addEventListener('keyup', () => {
+    
+    const term = search.value.trim().toLowerCase();
+
+    filterTodos(term)
+});
+
+
