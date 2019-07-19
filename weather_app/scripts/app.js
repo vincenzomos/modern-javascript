f4850e7efab62b3b70645b100c0c70da8e429e6f
Vincent Mos <vincenzomos@gmail.com>
2019-07-19 09:30:57 +0200

    Self done work lesson-101

---
+++ weather_app/scripts/app.js
@@ -0,0 +1,47 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+
+const updateUI = (data) => {
+    //destructuring properties
+    const { cityDets, weather } = data;
+
+    //update the details template
+    details.innerHTML = `<h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>`
+
+    //remove the d-none class if present
+    if (card.classList.contains('d-none')) {
+        card.classList.remove('d-none');
+    }
+}
+
+const updateCity = async (city) => {
+
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.Key);
+    return {
+        cityDets,
+        weather
+    };
+}
+
+cityForm.addEventListener('submit', e => {
+    //prevent default action
+    e.preventDefault();
+
+    //get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    //update the UI with the new city
+    updateCity(city)
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
+
+})
